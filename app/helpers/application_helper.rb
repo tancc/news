6dc8f0da90da9c7e0c4829dfb2a76e3a66cf5f77
Gary Lai <garylai1990@gmail.com>
2013-07-13 22:53:53 +0800

    set devise settings:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def markdown(text)
     options = {
       :autolink => true,
@@ -7,5 +8,20 @@ module ApplicationHelper
     }
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
     markdown.render(text).html_safe
+  end
+
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
   end
 end
