1ec7b2749a5ae989781e0492ddd437bf116db9d8
Gary Lai <garylai1990@gmail.com>
2013-07-13 12:18:14 +0800

    rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713041344) do
-
-  create_table "posts", :force => true do |t|
-    t.string   "title"
-    t.text     "content"
-    t.string   "source"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
