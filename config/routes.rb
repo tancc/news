b727ec629cb80030d30d369de649f5682e73c365
Gary Lai <garylai1990@gmail.com>
2013-07-20 15:44:45 +0800

    welcome_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 News::Application.routes.draw do
+  get "welcome/index"
+
   devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
   resources :posts
 
-  root :to => 'posts#index'
+  root :to => "welcome#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
