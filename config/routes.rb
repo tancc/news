c0d2c491c21a3208dcaf055f24bba04ae9143a35
Gary Lai <garylai1990@gmail.com>
2013-07-20 20:30:30 +0800

    routes, add namespace manage to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ News::Application.routes.draw do
   get "welcome/index"
 
   devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
-
-  resources :posts
+  namespace :manage do
+    resources :posts
+  end
 
   root :to => "welcome#index"
   # The priority is based upon order of creation:
